c71fac21010d49463b10a33203475ab6a27da124
Agus Garcia <agusss.garcia19@gmail.com>
2025-06-29 19:39:00 -0300

    fix(next.config.js): add ability to customize props

--- packages/react-essentials-commands/src/configs/createNextConfig/createNextConfig.types.ts
+++ packages/react-essentials-commands/src/configs/createNextConfig/createNextConfig.types.ts
@@ -4,6 +4,7 @@ import { type Func, type getPackageJSON } from "#src/utils";
 
 export type Input = [
   core: NonNullable<Awaited<ReturnType<typeof getPackageJSON>>["core"]>,
+  configs?: Partial<NextConfig> | Func<Partial<NextConfig>, [phase: string]>,
 ];
 
 export type Output = Func<NextConfig, [phase: string]>;
