cbce306ac2170f820bf6d2c87d8de1f63937012b
Agus Garcia <agusss.garcia19@gmail.com>
2025-06-30 10:51:47 -0300

    fix: exit with error when format not pass

--- packages/react-essentials-commands/src/_bin/check.ts
+++ packages/react-essentials-commands/src/_bin/check.ts
@@ -3,19 +3,11 @@ import { execute } from "#src/utils";
 import run from "./_run";
 
 export default async function check(): Promise<void> {
-  await run(
-    "check",
-    () =>
-      execute(
-        "next lint --cache-location ./node_modules/.eslintcache --cache-strategy content --dir .",
-        true,
-      ),
-    () =>
-      execute(
-        "prettier . --cache --cache-location ./node_modules/.prettiercache --cache-strategy content --check",
-        true,
-      ),
-    () => execute("tsc --pretty --noEmit", true),
+  await run("check", () =>
+    execute(
+      "next lint --cache-location ./node_modules/.eslintcache --cache-strategy content --max-warnings 0 --dir .",
+      true,
+    ),
   );
 }
 
