f25997fd345c6ce9297c6169f48e9535d0c91a68
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-03 10:17:47 -0300

    fix(check): adjust typescript checks

--- packages/react-essentials-commands/src/_bin/check.ts
+++ packages/react-essentials-commands/src/_bin/check.ts
@@ -6,11 +6,14 @@ import run from "./_run";
 
 export default async function check(): Promise<void> {
   try {
-    await run("check", () =>
-      execute(
-        "next lint --cache-location ./node_modules/.eslintcache --cache-strategy content --max-warnings 0 --dir .",
-        true,
-      ),
+    await run(
+      "check",
+      () =>
+        execute(
+          "next lint --cache-location ./node_modules/.eslintcache --cache-strategy content --max-warnings 0 --dir .",
+          true,
+        ),
+      () => execute("tsc --pretty --noEmit", true),
     );
   } catch (error) {
     console.error(getErrorMessage(error));
