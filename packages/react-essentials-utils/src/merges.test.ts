dc5a58c55085c908053dc7be97477b8fb422217c
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-03 06:53:04 -0300

    fix(merges): stop using sort

--- packages/react-essentials-utils/src/merges.test.ts
+++ packages/react-essentials-utils/src/merges.test.ts
@@ -111,41 +111,23 @@ describe("merges", () => {
       });
     });
 
-    it("should handle shallow merging with a boolean sort parameter set to true", () => {
+    it("should handle shallow merging", () => {
       const base = { a: ["z", "x", "y"] };
       const next = { a: ["b", "a", "c"] };
-      expect(merges.shallow(base, next, { level: 2, sort: true })).toEqual({
-        a: ["a", "b", "c", "x", "y", "z"],
-      });
-    });
-
-    it("should handle shallow merging with a boolean sort parameter set to false", () => {
-      const base = { a: ["z", "x", "y"] };
-      const next = { a: ["b", "a", "c"] };
-      expect(merges.shallow(base, next, { level: 2, sort: false })).toEqual({
+      expect(merges.shallow(base, next, { level: 2 })).toEqual({
         a: ["z", "x", "y", "b", "a", "c"],
       });
     });
 
-    it("should handle shallow merging with a custom sort function", () => {
-      const base = { a: [3, 1, 2] };
-      const next = { a: [6, 5, 4] };
-      const sortFn = (x: number, y: number) => x - y;
-      expect(merges.shallow(base, next, { level: 2, sort: sortFn })).toEqual({
-        a: [1, 2, 3, 4, 5, 6],
-      });
-    });
-
-    it("should handle shallow merging with nested arrays and a custom sort function", () => {
+    it("should handle shallow merging with nested arrays", () => {
       const base = { a: { b: [9, 7, 8] } };
       const next = { a: { b: [6, 5, 4] } };
-      const sortFn = (x: number, y: number) => x - y;
-      expect(merges.shallow(base, next, { level: 3, sort: sortFn })).toEqual({
-        a: { b: [4, 5, 6, 7, 8, 9] },
+      expect(merges.shallow(base, next, { level: 3 })).toEqual({
+        a: { b: [9, 7, 8, 6, 5, 4] },
       });
     });
 
-    it("should handle shallow merging with arrays of objects and a custom sort function", () => {
+    it("should handle shallow merging with arrays of objects", () => {
       const base = {
         a: [
           { id: 3, value: "c" },
@@ -158,30 +140,21 @@ describe("merges", () => {
           { id: 4, value: "d" },
         ],
       };
-      const sortFn = (x: any, y: any) => x.id - y.id;
-      expect(merges.shallow(base, next, { level: 2, sort: sortFn })).toEqual({
+      expect(merges.shallow(base, next, { level: 2 })).toEqual({
         a: [
+          { id: 3, value: "c" },
           { id: 1, value: "a" },
           { id: 2, value: "b" },
-          { id: 3, value: "c" },
           { id: 4, value: "d" },
         ],
       });
     });
 
-    it("should handle shallow merging with mixed data types and a custom sort function", () => {
+    it("should handle shallow merging with mixed data types", () => {
       const base = { a: [3, "b", 1] };
       const next = { a: [2, "a", 4] };
-      const sortFn = (x: any, y: any) => {
-        if (typeof x === "number" && typeof y === "string") return -1;
-        if (typeof x === "string" && typeof y === "number") return 1;
-        if (typeof x === "number" && typeof y === "number") return x - y;
-        if (typeof x === "string" && typeof y === "string")
-          return x.localeCompare(y);
-        return 0;
-      };
-      expect(merges.shallow(base, next, { level: 2, sort: sortFn })).toEqual({
-        a: [1, 2, 3, 4, "a", "b"],
+      expect(merges.shallow(base, next, { level: 2 })).toEqual({
+        a: [3, "b", 1, 2, "a", 4],
       });
     });
   });
@@ -261,32 +234,23 @@ describe("merges", () => {
       });
     });
 
-    it("should deeply merge objects with a custom sort function", () => {
+    it("should deeply merge objects", () => {
       const base = { a: [3, 1, 2] };
       const next = { a: [6, 5, 4] };
-      const sortFn = (x: number, y: number) => x - y;
-      expect(merges.deep(base, next, { sort: sortFn })).toEqual({
-        a: [1, 2, 3, 4, 5, 6],
+      expect(merges.deep(base, next)).toEqual({
+        a: [3, 1, 2, 6, 5, 4],
       });
     });
 
-    it("should deeply merge objects with a boolean sort parameter set to true", () => {
+    it("should deeply merge objects", () => {
       const base = { a: ["z", "x", "y"] };
       const next = { a: ["b", "a", "c"] };
-      expect(merges.deep(base, next, { sort: true })).toEqual({
-        a: ["a", "b", "c", "x", "y", "z"],
-      });
-    });
-
-    it("should deeply merge objects with a boolean sort parameter set to false", () => {
-      const base = { a: ["z", "x", "y"] };
-      const next = { a: ["b", "a", "c"] };
-      expect(merges.deep(base, next, { sort: false })).toEqual({
+      expect(merges.deep(base, next)).toEqual({
         a: ["z", "x", "y", "b", "a", "c"],
       });
     });
 
-    it("should deeply merge arrays of objects with a custom comparator and sort function", () => {
+    it("should deeply merge arrays of objects with a custom comparator", () => {
       const base = {
         a: [
           { id: 3, value: "c" },
@@ -300,25 +264,21 @@ describe("merges", () => {
         ],
       };
       const comparator = (x: any, y: any) => x.id === y.id;
-      const sortFn = (x: any, y: any) => x.id - y.id;
-      expect(
-        merges.deep(base, next, { array: { comparator }, sort: sortFn }),
-      ).toEqual({
+      expect(merges.deep(base, next, { array: { comparator } })).toEqual({
         a: [
+          { id: 3, value: "c" },
           { id: 1, value: "a" },
           { id: 2, value: "b" },
-          { id: 3, value: "c" },
           { id: 4, value: "d" },
         ],
       });
     });
 
-    it("should deeply merge objects with nested arrays and a custom sort function", () => {
+    it("should deeply merge objects with nested arrays", () => {
       const base = { a: { b: [9, 7, 8] } };
       const next = { a: { b: [6, 5, 4] } };
-      const sortFn = (x: number, y: number) => x - y;
-      expect(merges.deep(base, next, { sort: sortFn })).toEqual({
-        a: { b: [4, 5, 6, 7, 8, 9] },
+      expect(merges.deep(base, next)).toEqual({
+        a: { b: [9, 7, 8, 6, 5, 4] },
       });
     });
   });
