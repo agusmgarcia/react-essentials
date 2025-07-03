bed4e359d016eba4a48cbb16bbec3c93a972eda6
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-03 06:53:04 -0300

    feat(merges): stop using sort

--- packages/react-essentials-utils/src/merges.ts
+++ packages/react-essentials-utils/src/merges.ts
@@ -1,7 +1,6 @@
 import * as equals from "./equals";
 import * as filters from "./filters";
 import type Func from "./Func.types";
-import * as sorts from "./sorts";
 
 /**
  * Merges two objects or arrays strictly, ensuring deep equality checks.
@@ -11,7 +10,7 @@ import * as sorts from "./sorts";
  * @returns The merged result.
  */
 export function strict(base: unknown, next: unknown): any {
-  return merge(base, next, 0, defaultSort, equals.deep);
+  return merge(base, next, 0, equals.deep);
 }
 
 /**
@@ -24,7 +23,6 @@ export function strict(base: unknown, next: unknown): any {
  *   - An object with optional properties:
  *     - `array.comparator`: A custom comparator function for arrays.
  *     - `level`: The depth level for merging.
- *     - `sort`: A custom sorting function.
  * @returns The merged result.
  */
 export function shallow(
@@ -35,7 +33,6 @@ export function shallow(
     | {
         array?: { comparator?: ArrayComparatorFn };
         level?: number;
-        sort?: SortFn | boolean;
       } = 1,
 ): any {
   return merge(
@@ -46,15 +43,6 @@ export function shallow(
       : typeof options.level === "number"
         ? options.level
         : 1,
-    typeof options === "number"
-      ? defaultUnSort
-      : typeof options.sort === "undefined"
-        ? defaultUnSort
-        : typeof options.sort === "boolean"
-          ? options.sort
-            ? defaultSort
-            : defaultUnSort
-          : options.sort,
     typeof options === "number"
       ? equals.deep
       : options.array?.comparator || equals.deep,
@@ -68,7 +56,6 @@ export function shallow(
  * @param next - The next object or array to merge from.
  * @param options - Optional merge options:
  *   - `array.comparator`: A custom comparator function for arrays.
- *   - `sort`: A custom sorting function.
  * @returns The merged result.
  */
 export function deep(
@@ -76,20 +63,12 @@ export function deep(
   next: unknown,
   options?: {
     array?: { comparator?: ArrayComparatorFn };
-    sort?: SortFn | boolean;
   },
 ): any {
   return merge(
     base,
     next,
     undefined,
-    typeof options?.sort === "undefined"
-      ? defaultUnSort
-      : typeof options.sort === "boolean"
-        ? options.sort
-          ? defaultSort
-          : defaultUnSort
-        : options.sort,
     options?.array?.comparator || equals.deep,
   );
 }
@@ -98,7 +77,6 @@ function merge(
   base: any,
   next: any,
   level: number | undefined,
-  sort: SortFn,
   arrayComparator: ArrayComparatorFn,
 ): any {
   if (!!level && level < 0) return next;
@@ -111,7 +89,6 @@ function merge(
     if (!Array.isArray(base)) base = [];
 
     return [...base, ...next]
-      .sort(sort)
       .filter(filters.distinct(arrayComparator))
       .map((baseValue) => {
         const index = next.findIndex((nextValue) =>
@@ -119,8 +96,8 @@ function merge(
         );
 
         return index !== -1
-          ? merge(baseValue, next[index], followingLevel, sort, arrayComparator)
-          : merge(undefined, baseValue, followingLevel, sort, arrayComparator);
+          ? merge(baseValue, next[index], followingLevel, arrayComparator)
+          : merge(undefined, baseValue, followingLevel, arrayComparator);
       });
   }
 
@@ -128,26 +105,13 @@ function merge(
     if (Array.isArray(base) || typeof base !== "object" || !base) base = {};
 
     return [...Object.keys(base), ...Object.keys(next)]
-      .sort(sort)
       .filter(filters.distinct)
       .reduce(
         (result, key) => {
           result[key] =
             key in next
-              ? merge(
-                  base[key],
-                  next[key],
-                  followingLevel,
-                  sort,
-                  arrayComparator,
-                )
-              : merge(
-                  undefined,
-                  base[key],
-                  followingLevel,
-                  sort,
-                  arrayComparator,
-                );
+              ? merge(base[key], next[key], followingLevel, arrayComparator)
+              : merge(undefined, base[key], followingLevel, arrayComparator);
 
           return result;
         },
@@ -156,34 +120,6 @@ function merge(
   }
 
   return next;
-}
-
-type SortFn = Func<number, [element1: any, element2: any]>;
-
-function defaultUnSort(): number {
-  return 0;
-}
-
-function defaultSort(element1: any, element2: any): number {
-  if (typeof element1 !== typeof element2) return 0;
-
-  switch (typeof element1) {
-    case "number":
-      return sorts.byNumberAsc(element1, element2 as number);
-
-    case "boolean":
-      return sorts.byBooleanAsc(element1, element2 as boolean);
-
-    case "bigint":
-    case "function":
-    case "object":
-    case "symbol":
-    case "undefined":
-      return 0;
-
-    case "string":
-      return sorts.byStringAsc(element1, element2 as string);
-  }
 }
 
 type ArrayComparatorFn = Func<boolean, [element1: any, element2: any]>;
