79dbe9b506a780ffa45c881b79c0234998158209
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-03 06:34:04 -0300

    feat: add ability to customize the error message when not found

--- packages/react-essentials-commands/src/utils/getErrorMessage.ts
+++ packages/react-essentials-commands/src/utils/getErrorMessage.ts
@@ -1,7 +1,17 @@
 import hasProperty from "./hasProperty";
 
-export default function getErrorMessage(error: unknown): string {
+export default function getErrorMessage(error: unknown): string | undefined;
+
+export default function getErrorMessage(
+  error: unknown,
+  notFound: string,
+): string;
+
+export default function getErrorMessage(
+  error: unknown,
+  notFound?: string | undefined,
+): string | undefined {
   if (typeof error === "string") return error;
   if (hasProperty(error, "message", "string")) return error.message;
-  return "Not deserializable error";
+  return notFound || "An unexpected error occurred";
 }
