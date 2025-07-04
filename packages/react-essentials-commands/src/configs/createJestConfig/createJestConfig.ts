0d38dbb1ab62ec6f6e8915a78d3cba4db745a58e
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-04 12:12:59 -0300

    feat(createJestConfig): add support for "_bin" folder for libs

--- packages/react-essentials-commands/src/configs/createJestConfig/createJestConfig.ts
+++ packages/react-essentials-commands/src/configs/createJestConfig/createJestConfig.ts
@@ -9,26 +9,69 @@ import { type Input, type Output } from "./createJestConfig.types";
  *               Supported values typically include "app", "lib", or others.
  * @returns A Jest configuration object compatible with Next.js and the specified core type.
  */
-export default function createJestConfig(...[core]: Input): Output {
-  const createJestConfig = nextJest({ dir: "./" });
-
-  return createJestConfig({
+export default async function createJestConfig(
+  ...[core]: Input
+): Promise<Output> {
+  const createNextJestConfig = nextJest({ dir: "./" })({
     cacheDirectory: "node_modules/.jestcache",
     clearMocks: true,
-    coverageProvider: "v8",
+    displayName: core !== "lib" ? "Source" : undefined,
     moduleNameMapper:
       core === "app"
         ? {
-            "^#public/(.*)$": "<rootDir>/public/$1",
-            "^#src/(.*)$": "<rootDir>/src/$1",
+            "^#public\\/(.*)$": "<rootDir>/public/$1",
+            "^#src\\/(.*)$": "<rootDir>/src/$1",
           }
         : {
-            "^#src/(.*)$": "<rootDir>/src/$1",
+            "^#src\\/(.*)$": "<rootDir>/src/$1",
           },
-    testEnvironment: core === "app" || core === "lib" ? "jsdom" : "node",
+    testEnvironment:
+      core === "app"
+        ? "jsdom"
+        : core === "azure-func"
+          ? "node"
+          : core === "lib"
+            ? undefined
+            : "node",
     testRegex:
       core === "app"
-        ? ["pages\\/.*\\.test\\.[jt]sx?$", "src\\/.*\\.test\\.[jt]sx?$"]
-        : ["src\\/.*\\.test\\.[jt]sx?$"],
-  } as Parameters<typeof createJestConfig>[0]);
+        ? [
+            "<rootDir>/pages/**/*.test.[jt]s?(x)",
+            "<rootDir>/src/**/*.test.[jt]s?(x)",
+          ]
+        : core === "azure-func"
+          ? ["<rootDir>/src/**/*.test.[jt]s"]
+          : core === "lib"
+            ? undefined
+            : ["<rootDir>/src/**/*.test.[jt]s"],
+  });
+
+  const nextJestConfig = await createNextJestConfig();
+
+  return {
+    ...nextJestConfig,
+    coverageProvider: "v8",
+    projects:
+      core === "lib"
+        ? [
+            {
+              ...nextJestConfig,
+              displayName: "Source",
+              testEnvironment: "jsdom",
+              testMatch: ["<rootDir>/src/**/*.test.[jt]s?(x)"],
+              testPathIgnorePatterns: [
+                ...(nextJestConfig.testPathIgnorePatterns || []),
+                "<rootDir>/src/_bin/",
+                "<rootDir>/node_modules/",
+              ],
+            },
+            {
+              ...nextJestConfig,
+              displayName: "Binaries",
+              testEnvironment: "node",
+              testMatch: ["<rootDir>/src/_bin/**/*.test.[jt]s?(x)"],
+            },
+          ]
+        : undefined,
+  };
 }
--- packages/react-essentials-commands/src/configs/createJestConfig/createJestConfig.types.ts
+++ packages/react-essentials-commands/src/configs/createJestConfig/createJestConfig.types.ts
@@ -1,4 +1,4 @@
-import type nextJest from "next/jest";
+import { type Config } from "jest";
 
 import { type getPackageJSON } from "#src/utils";
 
@@ -6,4 +6,4 @@ export type Input = [
   core: NonNullable<Awaited<ReturnType<typeof getPackageJSON>>["core"]>,
 ];
 
-export type Output = ReturnType<ReturnType<typeof nextJest>>;
+export type Output = Config;
