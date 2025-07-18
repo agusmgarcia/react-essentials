19a17a25060ba61ee22cd81b6aee2a38f5c20b80
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-18 14:56:21 -0300

    fix(createJestConfig): adjust testMatch

--- packages/react-essentials-commands/src/configs/createJestConfig/createJestConfig.ts
+++ packages/react-essentials-commands/src/configs/createJestConfig/createJestConfig.ts
@@ -33,7 +33,7 @@ export default async function createJestConfig(
           : core === "lib"
             ? undefined
             : "node",
-    testRegex:
+    testMatch:
       core === "app"
         ? [
             "<rootDir>/pages/**/*.test.[jt]s?(x)",
