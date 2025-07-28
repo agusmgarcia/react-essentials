52e3b596627d0ad8dd36d5b9856e2bf7c21af22e
Agus Garcia <agusss.garcia19@gmail.com>
2025-07-28 02:59:53 -0300

    feat(createNextConfig): remove configs

--- packages/react-essentials-commands/src/configs/createNextConfig/createNextConfig.types.ts
+++ packages/react-essentials-commands/src/configs/createNextConfig/createNextConfig.types.ts
@@ -11,12 +11,6 @@ export type Input = [
     NonNullable<Awaited<ReturnType<typeof getPackageJSON>>["core"]>,
     "app"
   >,
-  /**
-   * An optional config object or a function that returns a config object.
-   * If provided, it can be a partial Next.js configuration or a function that takes the
-   * current phase as an argument and returns a partial Next.js configuration.
-   */
-  configs?: Partial<NextConfig> | Func<Partial<NextConfig>, [phase: string]>,
 ];
 
 export type Output = Func<NextConfig, [phase: string]>;
