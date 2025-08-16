23bef72dc5772304cc4676cf32312b6394c8ac36
Agus Garcia <agusss.garcia19@gmail.com>
2025-08-16 13:02:11 -0300

    chore(LoadEnvConfigLoader): add doc

--- packages/react-essentials-commands/src/_out/LoadEnvConfigLoader.ts
+++ packages/react-essentials-commands/src/_out/LoadEnvConfigLoader.ts
@@ -2,6 +2,14 @@ import { type LoaderContext } from "webpack";
 
 import essentialsPackageJSON from "../../package.json";
 
+/**
+ * A custom Webpack loader that prepends an import statement for the `loadEnvConfig` module
+ * from the current package before the original source code. This ensures that environment
+ * configuration is loaded before any other code in the processed file.
+ *
+ * @param source - The original source code to be transformed.
+ * @returns The modified source code with the import statement prepended.
+ */
 export default function LoadEnvConfigLoader(
   this: LoaderContext<{}>,
   source: string,
