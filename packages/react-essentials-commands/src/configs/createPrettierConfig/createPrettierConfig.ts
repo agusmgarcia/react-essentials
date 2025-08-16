0aff60f01b59c7edc64c01a87273ebbb486dc156
Agus Garcia <agusss.garcia19@gmail.com>
2025-08-16 13:01:59 -0300

    chore(createPrettierConfig): adjust doc

--- packages/react-essentials-commands/src/configs/createPrettierConfig/createPrettierConfig.ts
+++ packages/react-essentials-commands/src/configs/createPrettierConfig/createPrettierConfig.ts
@@ -3,7 +3,8 @@ import { type Input, type Output } from "./createPrettierConfig.types";
 /**
  * Generates a Prettier configuration object based on the provided core type.
  *
- * @param core - The core type that determines the configuration. Supported values include "azure-func", "node", "app", and others.
+ * @param core - The core type that determines the configuration.
+ *               Supported values include "azure-func", "node", "app", and others.
  * @returns An empty object for "azure-func" or "node" core types, or a Prettier configuration object with Tailwind CSS plugin and related settings for other core types.
  */
 export default function createPrettierConfig(...[core]: Input): Output {
