ebfe1e72b3146f94248057f77e514db48cfd4da4
Agus Garcia <agusss.garcia19@gmail.com>
2025-10-29 12:25:45 -0300

    feat(Store): add equality function to filter devtools states

--- packages/react-essentials-store/src/Store/Store.setupDevTools.ts
+++ packages/react-essentials-store/src/Store/Store.setupDevTools.ts
@@ -1,14 +1,22 @@
 import type {} from "@redux-devtools/extension";
 
-import { errors, isSSR } from "@agusmgarcia/react-essentials-utils";
+import { equals, errors, isSSR } from "@agusmgarcia/react-essentials-utils";
 
 import { ServerSlice } from "../ServerSlice";
 import type Store from "./Store";
-import { type BaseSliceFactories, type StateOf } from "./Store.types";
+import {
+  type BaseSliceFactories,
+  type Configs,
+  type ReduxStateOf,
+  type StateOf,
+} from "./Store.types";
 
 export default function setupDevTools<
   TSliceFactories extends BaseSliceFactories,
->(store: Store<TSliceFactories>, devTools: boolean | string | undefined): void {
+>(
+  store: Store<TSliceFactories>,
+  devTools: Configs<TSliceFactories>["devTools"],
+): void {
   if (isSSR()) return;
   if (!window.__REDUX_DEVTOOLS_EXTENSION__) return;
   if (
@@ -18,7 +26,12 @@ export default function setupDevTools<
   )
     return;
 
-  const name = typeof devTools === "string" ? devTools : "Store";
+  const name =
+    typeof devTools === "string"
+      ? devTools
+      : typeof devTools === "undefined" || typeof devTools === "boolean"
+        ? "Store"
+        : devTools.name || "Store";
 
   window.__REDUX_DEVTOOLS_CONNECTIONS__ ||= {};
   const initialized = !!window.__REDUX_DEVTOOLS_CONNECTIONS__[name];
@@ -37,32 +50,39 @@ export default function setupDevTools<
         skip: false,
         test: false,
       },
-      name: typeof devTools === "string" ? devTools : "Store",
+      name,
     }));
 
-  if (!initialized) connection.init(extractState(store.state));
-  else connection.send({ type: "STATE_RESET" }, extractState(store.state));
+  let prevState = extractState(store.state);
+
+  if (!initialized) connection.init(prevState);
+  else connection.send({ type: "STATE_RESET" }, prevState);
+
+  const areEquals =
+    typeof devTools === "object"
+      ? devTools.equality || equals.deep
+      : equals.deep;
 
   store.subscribe((state) => {
     const newState = extractState(state);
+    if (areEquals(newState, prevState)) return;
+
+    prevState = newState;
     connection.send({ type: "STATE_UPDATED" }, newState);
   });
 }
 
 function extractState<TSliceFactories extends BaseSliceFactories>(
   state: StateOf<TSliceFactories>,
-): Record<string, any> {
-  return Object.keys(state).reduce(
-    (result, key) => {
-      const slice = state[key as keyof typeof state];
-      result[key] =
-        slice instanceof ServerSlice
-          ? { ...slice.state, error: errors.getMessage(slice.state.error) }
-          : slice.state;
-      return result;
-    },
-    {} as Record<string, any>,
-  );
+): ReduxStateOf<TSliceFactories> {
+  return Object.keys(state).reduce((result, key) => {
+    const slice = state[key as keyof typeof state];
+    result[key as keyof ReduxStateOf<TSliceFactories>] =
+      slice instanceof ServerSlice
+        ? { ...slice.state, error: errors.getMessage(slice.state.error) }
+        : slice.state;
+    return result;
+  }, {} as ReduxStateOf<TSliceFactories>);
 }
 
 declare global {
