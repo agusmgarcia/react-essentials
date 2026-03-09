71e44d1f7b04b7ff753336843714bc108f19014b
Agus Garcia <agusss.garcia19@gmail.com>
2026-03-09 07:35:56 -0300

    feat(createWebpackConfig): stop finding dependencies automatically

--- packages/react-essentials-commands/webpack.config.ts
+++ packages/react-essentials-commands/webpack.config.ts
@@ -1,3 +1,13 @@
 import { createWebpackConfig } from "./src/configs";
 
-export default createWebpackConfig("lib", { omit: "web" });
+export default createWebpackConfig("lib", {
+  externals: [
+    "eslint/config",
+    "eslint-config-next/core-web-vitals",
+    "eslint-config-next/typescript",
+    "eslint-plugin-prettier/recommended",
+    "next/constants",
+    "next/jest",
+  ],
+  omit: "web",
+});
