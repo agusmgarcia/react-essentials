874034784aac300e442cfedc1217ca5f9bf4b9d8
Agus Garcia <agusss.garcia19@gmail.com>
2026-04-28 03:03:39 -0300

    fix: adjust import order

--- packages/react-essentials-store/src/classes/index.ts
+++ packages/react-essentials-store/src/classes/index.ts
@@ -1,8 +1,9 @@
-export * from "./BrowserStorageSlice";
+// eslint-disable-next-line sort/exports
 export * from "./GlobalSlice";
+export * from "./ServerSlice";
+export * from "./StorageSlice";
+export * from "./BrowserStorageSlice";
 export * from "./LocalStorageSlice";
 export * from "./QueryStringStorageSlice";
-export * from "./ServerSlice";
 export * from "./SessionStorageSlice";
-export * from "./StorageSlice";
 export * from "./Store";
